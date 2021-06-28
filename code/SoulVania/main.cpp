b9e7805f1d0f480485af0eb5968b5cdfc70de4af
warzes <warzes@mail.ru>
2021-06-28 13:22:25 +1000

    - стартовый проект - glfw - raylib - тест

---
+++ code/SoulVania/main.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdafx.h"
+
+#if _WIN32
+// Prefer the high-performance GPU on switchable GPU systems
+extern "C"
+{
+    __declspec(dllexport) unsigned long NvOptimusEnablement = 1;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
+#pragma comment( lib, "winmm.lib" )
+#endif
+
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60); 
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello World!", 190, 200, 20, RED);
+
+        EndDrawing();
+    }
+    CloseWindow();
+}
---
+++ code/doc.md
@@ -0,0 +1,11 @@
+raylib
+	Commits on Jun 27, 2021
+		Minor tweaks
+
+		в raylib добавил 
+			#define PLATFORM_DESKTOP 1
+			#define GRAPHICS_API_OPENGL_33 1
+
+glfw
+	Commits on Jun 26, 2021
+		Remove errors for gamepad element mismatch Е
